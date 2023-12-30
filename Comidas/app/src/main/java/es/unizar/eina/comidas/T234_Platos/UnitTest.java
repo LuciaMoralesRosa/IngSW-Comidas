32be75430b401934cb214acce8ac6c6320c5b84f
Lucía <lucia.moralesrosa99@gmail.com>
2023-12-30 12:31:29 +0100

    UnitTest vacio PlatoRepository modificado - Compila

--- Comidas/app/src/main/java/es/unizar/eina/comidas/T234_Platos/UnitTest.java
+++ Comidas/app/src/main/java/es/unizar/eina/comidas/T234_Platos/UnitTest.java
@@ -1,4 +1,12 @@
 package es.unizar.eina.comidas.T234_Platos;
 
+import android.app.Application;
+
+
 public class UnitTest {
+    Application application;
+    public UnitTest(Application app){
+        super();
+        application = app;
+    }
 }
--- Comidas/app/src/main/java/es/unizar/eina/comidas/database/PlatoRepository.java
+++ Comidas/app/src/main/java/es/unizar/eina/comidas/database/PlatoRepository.java
@@ -2,10 +2,14 @@ package es.unizar.eina.comidas.database;
 
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.lifecycle.LiveData;
 
 import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Repositorio que actúa como una capa de abstracción entre las fuentes de datos (como la base de
@@ -22,7 +26,7 @@ public class PlatoRepository {
     /** LiveData que contiene la lista de todos los platos ordenados. */
     private LiveData<List<Plato>> mAllPlatos;
 
-    //private int mNumeroDePlatos;
+    private final long TIMEOUT = 15000;
 
     /*
     Note that in order to unit test the PlatoRepository, you have to remove the Application
@@ -40,9 +44,41 @@ public class PlatoRepository {
         ComidasRoomDatabase db = ComidasRoomDatabase.getDatabase(application);
         mPlatoDao = db.platoDao();
         mAllPlatos = mPlatoDao.getAllPlatos();
-    //    mNumeroDePlatos = mPlatoDao.getNumeroDePlatosDB();
     }
 
+
+    private Boolean validarPlato(Plato plato){
+        boolean valor = true;
+
+        String primero = "PRIMERO";
+        String segundo = "SEGUNDO";
+        String postre = "POSTRE";
+
+        String nombre = plato.getNombre();
+        String categoria = plato.getCategoria();
+        Double precioPlato = plato.getPrecio();
+
+        LiveData<List<Plato>> todosLosPlatosLD = mAllPlatos;
+        List<Plato> listaDePlatos = todosLosPlatosLD.getValue();
+
+        //Huecos vacios
+        if(nombre.isEmpty() || categoria.isEmpty()) {
+            valor = false;
+        } else if (!(categoria.equals(primero)
+                || categoria.equals(segundo) || categoria.equals(postre))) { //Categoria valida
+            valor = false;
+        } else if (precioPlato < 0) { //Precio valido
+            valor = false;
+        } else { //Nombre del plato unico en la tabla
+            for(Plato p : listaDePlatos){
+                if(nombre.equals(p.getNombre())){
+                    valor = false;
+                    break;
+                }
+            }
+        }
+        return valor;
+    }
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
@@ -55,10 +91,6 @@ public class PlatoRepository {
         return mAllPlatos;
     }
 
-    //public int getNumeroDePlatos(){
-    //    return mNumeroDePlatos;
-    //}
-
     /**
      * Inserta un nuevo plato en la base de datos utilizando un hilo de ejecución separado.
      *
@@ -66,13 +98,30 @@ public class PlatoRepository {
      * @return El identificador único del plato recién insertado.
      */
     public long insert(Plato plato) {
-        final long[] result = {0};
+        //final long[] result = {0};
+        AtomicLong result = new AtomicLong();
+        Semaphore resource = new Semaphore(0);
+
         // You must call this on a non-UI thread or your app will throw an exception. Room ensures
         // that you're not doing any long running operations on the main thread, blocking the UI.
         ComidasRoomDatabase.databaseWriteExecutor.execute(() -> {
-            result[0] = mPlatoDao.insert(plato);
+            //result[0] = mPlatoDao.insert(plato);
+            long value;
+            if(validarPlato(plato)){
+                value = mPlatoDao.insert(plato);
+            } else {
+                value = -1;
+            }
+            result.set(value);
+            resource.release();
         });
-        return result[0];
+        try {
+            resource.tryAcquire(TIMEOUT, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e){
+            Log.d("PlatoRepository", "InterruptedException en Insert: " + e.getMessage());
+        }
+        //return result[0];
+        return result.get();
     }
 
     /**
